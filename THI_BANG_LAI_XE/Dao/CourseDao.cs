03238869b5d69b27c0553b58ac2110dc74e67d2f
Thinhnthe172159 <101821797+Thinhnthe172159@users.noreply.github.com>
2025-02-27 23:15:29 +0700

    Add Course dao and ExampaperDao

--- THI_BANG_LAI_XE/Dao/CourseDao.cs
+++ THI_BANG_LAI_XE/Dao/CourseDao.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Client;
 using THI_BANG_LAI_XE.Models;
 
@@ -13,7 +14,10 @@ namespace THI_BANG_LAI_XE.Dao
         private static ThiBangLaiXeContext _context = new ThiBangLaiXeContext();
 
         // get course list
-        public static List<Course> GetCourseList() => _context.Courses.ToList();
+        public static async Task<List<Course>> GetCourseList() => await _context.Courses.ToListAsync();
+
+        //Filter Course by custom field
+
 
         // Add course
         public static void AddCourse(Course course)
@@ -30,12 +34,41 @@ namespace THI_BANG_LAI_XE.Dao
         }
 
         //get course by id
-        public static Course? GetCourseById(int courseId) => _context.Courses.FirstOrDefault(c => c.CourseId == courseId);
+        public static async Task<Course?> GetCourseById(int courseId) => await _context.Courses.FirstOrDefaultAsync(c => c.CourseId == courseId);
 
         //Update course
-        public static void UpdateCourse()
+        public static async Task UpdateCourse(Course course)
         {
+            try
+            {
+                var UpdateCourse = await GetCourseById(course.CourseId);
+                if (UpdateCourse != null)
+                {
+                    UpdateCourse.CourseName = course.CourseName;
+                    UpdateCourse.TeacherId = course.TeacherId;
+                    UpdateCourse.StartDate = course.StartDate;
+                    UpdateCourse.EndDate = course.EndDate;
+                    UpdateCourse.ModifiedDate = DateTime.Now;
+                    _context.Courses.Update(UpdateCourse);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (Exception) { }
+        }
 
+        //Delete Course
+        public async Task RemoveCourse(int courseId)
+        {
+            try
+            {
+                var DeleteCourse = await GetCourseById(courseId);
+                if (DeleteCourse != null)
+                {
+                    _context.Courses.Remove(DeleteCourse);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (Exception) { }
         }
     }
 }
---
+++ THI_BANG_LAI_XE/Dao/ExamPaperDao.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Microsoft.Identity.Client;
+using THI_BANG_LAI_XE.Models;
+
+namespace THI_BANG_LAI_XE.Dao
+{
+    public class ExamPaperDao
+    {
+        private static ThiBangLaiXeContext _context = new ThiBangLaiXeContext();
+
+        //get exampaper list
+        public static async Task<List<ExamPaper>> getExamPaperList() => await _context.ExamPapers.ToListAsync();
+
+        //get exam paper by id
+        public static async Task<ExamPaper?> getExamPaperById(int id) => await _context.ExamPapers.FirstOrDefaultAsync(ex => ex.ExamPaperId == id);
+
+        //add examPaper 
+        public static async Task AddExamPaper(ExamPaper examPaper)
+        {
+            try
+            {
+                _context.ExamPapers.Add(examPaper);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        // Update Exam paper
+        public static async Task UpdateExamPaper(ExamPaper examPaper)
+        {
+            try
+            {
+                var ExamPaperToUpdate = await getExamPaperById(examPaper.ExamPaperId);
+                if (ExamPaperToUpdate != null)
+                {
+                    ExamPaperToUpdate.ExamPaperName = examPaper.ExamPaperName;
+                    _context.ExamPapers.Update(ExamPaperToUpdate);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        //Remove Exampaper
+        public static async Task RemoveExamPaper(int ExamPaperById)
+        {
+            try
+            {
+                var ExamPaperToRemove = await getExamPaperById(ExamPaperById);
+                if (ExamPaperToRemove != null)
+                {
+                    _context.ExamPapers.Remove(ExamPaperToRemove);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
