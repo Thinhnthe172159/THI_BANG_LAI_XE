5c08060dcd592ac656c1a1c1f1eb8c2d0cea1ba5
cuong <khdtv1208@gmail.com>
2025-03-23 17:42:11 +0700

    add notification, certificate, updateaccount, officer
    add and update

---
+++ THI_BANG_LAI_XE/View/Officer/AccountManagerPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using THI_BANG_LAI_XE.Dao;
+using THI_BANG_LAI_XE.Models;
+
+namespace THI_BANG_LAI_XE.View.Officer
+{
+    public partial class AccountManagerPage : Page
+    {
+        private ObservableCollection<User> accountList;
+        private UserDao userDao;
+        private ThiBangLaiXeContext dbSet;
+        private List<Role> roleList;
+
+        public AccountManagerPage()
+        {
+            InitializeComponent();
+            dbSet = new ThiBangLaiXeContext();
+            userDao = new UserDao(dbSet);
+            accountList = new ObservableCollection<User>();
+            roleList = new List<Role>();
+
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            // Lấy acc trong db
+            var users = userDao.GetUserList().ToList();
+            accountList = new ObservableCollection<User>(users);
+            DataAccount.ItemsSource = accountList;
+
+            // role trong db
+            roleList = dbSet.Roles.ToList();
+            ListRole.ItemsSource = roleList;
+            ListRole.DisplayMemberPath = "RoleName";
+        }
+
+        private void FilterByName(object sender, TextChangedEventArgs e)
+        {
+            var filteredList = accountList.Where(u => u.FullName.Contains(txtFullName.Text, StringComparison.OrdinalIgnoreCase)).ToList();
+            DataAccount.ItemsSource = filteredList;
+        }
+
+        private void FilterBySchoolName(object sender, TextChangedEventArgs e)
+        {
+            var filteredList = accountList.Where(u => u.School != null && u.School.Contains(txtSchool.Text, StringComparison.OrdinalIgnoreCase)).ToList();
+            DataAccount.ItemsSource = filteredList;
+        }
+        private void ListRole_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ListRole.SelectedItem is Role selectedRole)
+            {
+                var filteredList = accountList.Where(u => u.RoleNavigation.RoleName == selectedRole.RoleName).ToList();
+                DataAccount.ItemsSource = filteredList;
+            }
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            txtFullName.Text = "";
+            txtSchool.Text = "";
+            ListRole.SelectedItem = null;
+            DataAccount.ItemsSource = accountList;
+        }
+
+        private void DataAccount_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DataAccount.SelectedItem is User selectedUser)
+            {
+                UpdateAccountPage updatePage = new UpdateAccountPage(selectedUser);
+                updatePage.AccountUpdated += UpdatePage_AccountUpdated;
+                this.NavigationService?.Navigate(updatePage);
+            }
+        }
+
+        private void UpdatePage_AccountUpdated(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
---
+++ THI_BANG_LAI_XE/View/Officer/CertificatePage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using THI_BANG_LAI_XE.Models;
+
+namespace THI_BANG_LAI_XE.View.Officer
+{
+    public partial class CertificatePage : Page
+    {
+        private ThiBangLaiXeContext dbSet;
+        private User _user; // Thêm biến User
+
+        // Sửa constructor để nhận User
+        public CertificatePage(User user)
+        {
+            InitializeComponent();
+            dbSet = new ThiBangLaiXeContext();
+            _user = user;
+            LoadName();
+        }
+
+        private void LoadName()
+        {
+            try
+            {
+                // Sử dụng thông tin User đã được truyền
+                if (_user != null)
+                {
+                    txtName.Text = _user.FullName;
+                }
+                else
+                {
+                    MessageBox.Show("Không có thông tin người dùng.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi tải tên: {ex.Message}");
+            }
+        }
+    }
+}
